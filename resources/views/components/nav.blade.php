587c3bbc7668e9cbfafa623a675b26fbc9d3d462
Armando Gennaro Zadolinnyi <facebook2001@libero.it>
2022-07-29 17:56:03 +0200

    layout+bootstrap

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,52 @@
+<div class="myContainer">
+    <div class="navbar">
+        <div class="menu">
+            <h3 class="logo">Brand<span>Name</span></h3>
+            <div class="menu-2">
+                <div class="bar"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="main-container">
+    <div class="main">
+        <header>
+            <div class="overlay">
+                <div class="inner">
+                    <h2 class="title">abc</h2>
+                    <br>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, assumenda? Alias quos porro maiores. Id beatae deserunt atque facere? Quisquam possimus corporis eaque recusandae, porro repellat maxime sequi quod explicabo?
+                    </p>
+                    <br>
+                    <a href="#" class="btn">read more</a>
+                </div>
+            </div>
+        </header>
+    </div>
+</div>
+<div class="shadow one"></div>
+<div class="shadow two"></div>
+<div class="links">
+    <ul>
+        <li>
+            <a href="#" style="--i:0.5s">Home</a>
+        </li>
+        <li>
+            <a href="#" style="--i:0.10s">Service</a>
+        </li>
+        <li>
+            <a href="#" style="--i:0.15s">Portfolio</a>
+        </li>
+        <li>
+            <a href="#" style="--i:0.20s">Testimonials</a>
+        </li>
+        <li>
+            <a href="#" style="--i:0.25s">About</a>
+        </li>
+        <li>
+            <a href="#" style="--i:0.30s">Contact</a>
+        </li>
+
+    </ul>
+</div>
